383a52d1f6e617da83b8d204cc1c42880b8e87db
Kokwiri <109506391+Kokwiri@users.noreply.github.com>
2022-10-18 15:28:25 +0300

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,10 +6,11 @@
 
 void print_alphabet(void)
 {
-    int j;
-    for (j = 'a'; j <= 'z'; j++)
-    {
+   int j;
+for (j = 'a'; j <= 'z'; j++)
+{
         _putchar('j');
-    }
-    _putchar('\n');
+}
+_putchar('\n');
+
 }
