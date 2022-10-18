ce2604058dfeec69766a4dedd3674283e6e58398
Kokwiri <109506391+Kokwiri@users.noreply.github.com>
2022-10-18 21:03:04 +0300

    task7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,9 +9,9 @@ int print_last_digit(int)
 {
 int x;
 
-    if (n < 0)
-    n = -n;
-    x = n % 10;
-    _putchar(x + '0')
-    return (x);
+if (n < 0)
+n = -n;
+x = n % 10;
+_putchar(x + '0')
+return (x);
 }
