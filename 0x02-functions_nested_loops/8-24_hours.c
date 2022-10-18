39a463e8fe41dfc5d1e3bcff7e5232741dafeb1b
Kokwiri <109506391+Kokwiri@users.noreply.github.com>
2022-10-18 21:42:41 +0300

    task8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 
 /**
  * jack_bauer -> prints 24 hours
- * 
+ *
 */
 void jack_bauer(void)
 {
@@ -11,31 +11,31 @@ for (i = 0; i < 24; i++)
 {
 for (j = 0; j < 60; j++)
 {
-    if (i < 10)
-    {
-        _putchar('0');
-        _putchar(i + '0');
-    }
-    else if(i > 10)
-    {
-        _putchar((i / 10) + '0');
-        _putchar((i % 10) + '0');
-    }
-    if (j < 10)
-    {
-        _putchar(':');
-        _putchar('0');
-        _putchar(j + '0');
-    }
-    else if(j > 10)
-    {
-       _putchar(':');
-        _putchar((j / 10) + '0');
-        _putchar((j % 10) + '0');
-    }
-    _putchar(i);
-    _putchar(':');
-    _putchar(j);
+if (i < 10)
+{
+_putchar('0');
+_putchar(i + '0');
+}
+else if(i > 10)
+{
+_putchar((i / 10) + '0');
+_putchar((i % 10) + '0');
+}
+if (j < 10)
+{
+_putchar(':');
+_putchar('0');
+_putchar(j + '0');
+}
+else if(j > 10)
+{
+_putchar(':');
+_putchar((j / 10) + '0');
+_putchar((j % 10) + '0');
+}
+_putchar(i);
+_putchar(':');
+_putchar(j);
 }
 }
 }
