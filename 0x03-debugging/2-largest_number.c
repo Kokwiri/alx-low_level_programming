6cb27b4f13d9c499e5e4d4eae94212c2be912a35
Kokwiri <109506391+Kokwiri@users.noreply.github.com>
2022-10-19 23:24:29 +0300

    task2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -5,7 +5,7 @@
  * @a: first integer
  * @b: second integer
  * @c: third integer
- * Return: 0
+ * Return: largest number
  */
 
 int largest_number(int a, int b, int c)
@@ -25,5 +25,5 @@ int largest_number(int a, int b, int c)
         largest = c;
     }
 
-    return (0);
+    return (largest);
 }
