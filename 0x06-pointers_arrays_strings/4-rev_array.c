d7c56c686559be9828f79166be1d4254df8959b4
Kokwiri <109506391+Kokwiri@users.noreply.github.com>
2022-10-27 18:58:04 +0300

    task4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,7 +8,9 @@
 void reverse_array(int *a, int n)
 {
 	int *p, i, aux, k;
-
+a = [1, 2, 3, 4];
+aux = 1
+a = [4, 3, 2, 1 ]
 	p = a;
 	for (i = 0; i < n; i++)
 		p++;
@@ -18,5 +20,5 @@ void reverse_array(int *a, int n)
 		a[k] = *p;
 		*p = aux;
 		p--;
-	}
+	 }
 }
